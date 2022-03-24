c35e4d8bcf39eb7564ad3cad304468394b67678d
eduardocardona93 <eduardoacm93@gmail.com>
2022-03-24 14:19:07 -0400

    crud user

--- models/user.model.js
+++ models/user.model.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    minlength: 3
-  },
+    address:{type: String,required: true,unique: true,trim: true,minlength: 3},
+    email:{type: String,required: true,unique: true,trim: true,minlength: 3},
+    id:{type: String,required: true,unique: true,trim: true,minlength: 3},
+    isAdmin:{type: Boolean,default: false},
+    name:{type: String,required: true,unique: true,trim: true,minlength: 3},
+    password:{type: String,required: true,unique: true,trim: true,minlength: 3},
+    phoneNumber:{type: String,required: true,unique: true,trim: true,minlength: 3},
 }, {
   timestamps: true,
 });
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,63 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
+// GET ALL
 router.route('/').get((req, res) => {
   User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
-  const username = req.body.username;
 
-  const newUser = new User({username});
+//GET ONE
+router.route('/:id').get((req, res) => {
+  User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//DETELTE ONE
+router.route('/:id').delete((req, res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(() => res.json('User deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+// CREATE 
+router.route('/add').post((req, res) => {
+  const newUser = new User({
+    'address' : req.body.address,
+    'email' : req.body.email,
+    'id' : req.body.id,
+    'isAdmin' : req.body.isAdmin,
+    'name' : req.body.name,
+    'password' : req.body.password,
+    'phoneNumber' : req.body.phoneNumber,
+  });
 
   newUser.save()
     .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+// UPDATE
+router.route('/update/:id').post((req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+
+      user['address'] = req.body.address;
+      user['email'] = req.body.email;
+      user['id'] = req.body.id;
+      user['isAdmin'] = req.body.isAdmin;
+      user['name'] = req.body.name;
+      user['password'] = req.body.password;
+      user['phoneNumber'] = req.body.phoneNumber;
+      user.save()
+        .then(() => res.json('User updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
