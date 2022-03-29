804c822e14a096ca18c0227dbe3148da9e4aad12
eduardocardona93 <eduardoacm93@gmail.com>
2022-03-29 12:53:04 -0400

    going back

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const path = require('path');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-
+app.use(express.static("views"));
 app.use(cors());
 app.options('*', cors());
 app.use(express.json());
@@ -27,19 +26,11 @@ const productsInOrdersRouter = require('./routes/productsInOrder');
 const ordersRouter = require('./routes/orders');
 
 app.use('/users', usersRouter);
-app.use('/user', usersRouter);
 app.use('/products', productsRouter);
 app.use('/productsCategories', productsCategoriesRouter);
 app.use('/productsInOrders', productsInOrdersRouter);
 app.use('/orders', ordersRouter);
-app.use(express.static("views"));
-app.use('/public', express.static(path.join(__dirname, '/')));
-// Handle React routing, return all requests to React app
-app.get('/recoverPassword', function(req, res) {
-  res.sendFile(path.join(__dirname, 'views', 'recoverPassword.html'),(err) => {
-    if (err) res.sendStatus(404);
-});
-    });
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
